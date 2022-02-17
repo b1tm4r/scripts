6ee739e6d743a01282a241bf85e7b46462bf855e
Mariano M <63791682+kr1mz1337@users.noreply.github.com>
2022-02-17 11:06:28 +0100

    Create hash-password.py

---
+++ python/hash/hash-password.py
@@ -0,0 +1,13 @@
+#password testing: chg3p4sswd 
+import hashlib
+md5 = "2b51a4287ea2c750614f9eccfd505416"
+with open("wordlist.txt") as fname:
+        lineas = fname.readlines()
+        print(hashlib.md5(lineas[0].encode()))
+        for linea in lineas:
+                string = linea.replace('\n','') + "p4sswd"
+                try:
+                        if (hashlib.md5(string.encode('utf-8')).hexdigest() == md5):
+                                print string
+                except:
+                        continue
